f78ab0f8c1f5019a7d9e84a2937adbefa8de5625
alanmaranto <alanarostegui@hotmail.com>
2019-11-18 19:48:04 -0600

    get petition

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "ad1b8912";
+  const APP_KEY = "a68aad7b9632c4f8a2c6ee52ad806f15";
+
+  const Request = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect( () => {
+    getRecipies()
+  }, []);
+
+  const getRecipies = async () => {
+    const response = await fetch(Request)
+    const data = await response.json()
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit" >
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
